bd9f3cd25cb2c6d68b6db11615c806de629e2b19
flue fly <cuimmcan@qq.com>
2018-10-28 23:02:37 +0800

    router-acrtive

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link exact to="/">Home</router-link> |
+      <router-link exact to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 <style lang="stylus">
+#app
+  text-align center
+  .router-link-exact-active
+    color #1873e0
 </style>
